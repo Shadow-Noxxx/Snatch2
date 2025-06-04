9dc39442df13a91daf45c5433ead4354f4e09a69
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-04 23:26:11 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -58,7 +58,13 @@ async def start(update: Update, context: CallbackContext) -> None:
         
         
         caption = start_msg
-        reply_markup = keyboard
+           keyboard = [
+            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("HELP", callback_data='help')]
+        ]
+        reply_markup = InlineKeyboardButton(keyboard) 
         photo_url = random.choice(PHOTO_URL)
 
         await context.bot.send_photo(chat_id=update.effective_chat.id, photo=photo_url, caption= start_msg , reply_markup=reply_markup, parse_mode='markdown')
