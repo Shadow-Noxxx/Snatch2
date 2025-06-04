447971a868978286a679600b41cd8760a95d12d6
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-04 23:31:19 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -58,10 +58,10 @@ async def start(update: Update, context: CallbackContext) -> None:
         
         
         caption = start_msg
-           keyboard = [
+        keyboard = [
             [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')]
         ]
         reply_markup = InlineKeyboardButton(keyboard) 
