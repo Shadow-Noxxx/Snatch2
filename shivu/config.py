9eaf135e322d89124c6147c6a43f15e21c7aa4f9
Shadow-Noxxx <jangraji518@gmail.com>
2025-06-28 12:23:35 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "7819315360" , "6138142369"
     GROUP_ID = "-1002003248653"
     TOKEN = "7429550304:AAEFhObQ2DsKF8H9di9bVkPb_dZIZufnLy8"
-    mongo_url = "mongodb+srv://TEAMBABY01:UTTAMRATHORE09@cluster0.vmjl9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    mongo_url = "mongodb+srv://<shadow>:<shadow123>@cluster0.4qbhmds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "MidexozSupport"
     UPDATE_CHAT = "MidexozBotUpdates"
